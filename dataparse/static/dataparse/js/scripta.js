0ecab5b8107f6d4d8ba71554162cc31f48b274b1
Andy Kuck <andy@DESKTOP-DKLKKGG.localdomain>
2020-08-20 05:22:41 -0500

    Basic framework of site running, without much actual parsing yet

--- dataparse/static/dataparse/js/scripta.js
+++ dataparse/static/dataparse/js/scripta.js
@@ -5,11 +5,22 @@ function slide(direction) {
 	var slideButton2 = document.querySelector(".expand-icon-2");
 	if (direction == 'right') {
 		topLeft.style.display = 'flex';
-		slideButton1.style.display = 'block';
-		slideButton2.style.display = 'none';
+		slideButton1.removeAttribute('style');
+		slideButton2.removeAttribute('style');
 	} else {              
 		topLeft.style.display = 'none';
 		slideButton1.style.display = 'none';
 		slideButton2.style.display = 'block';
+	}
+}
+
+function scrollToTop() {
+	var indicator = document.querySelector(".fade-panel")
+	var position = window.getComputedStyle(indicator).getPropertyValue("position");
+	
+	if ((position == 'sticky') && (window.scrollY >= 162)) {
+		window.scrollTo(0, 162);
+	} else {
+		window.scrollTo(0, 0);
 	}
 }
