c3b3adb89f2c92d99064b8e8dc1b1ae30d3e6418
Andy Kuck <andy@DESKTOP-DKLKKGG.localdomain>
2020-08-21 07:46:00 -0500

    Added JS logic and additional style

--- dataparse/static/dataparse/js/scripta.js
+++ dataparse/static/dataparse/js/scripta.js
@@ -1,4 +1,7 @@
 
+var tabData = [1,0,0,0]
+var indicator = document.querySelector(".fade-panel")
+
 function slide(direction) {
 	var topLeft = document.querySelector(".top-left"); 
 	var slideButton1 = document.querySelector(".expand-icon-1");
@@ -14,13 +17,49 @@ function slide(direction) {
 	}
 }
 
-function scrollToTop() {
-	var indicator = document.querySelector(".fade-panel")
+function switchTab(tabInt) {
+	if (tabInt == 1) {
+		$('#tab-one').tab('show')
+	} else if (tabInt == 2) {
+		$('#tab-two').tab('show')
+	} else if (tabInt == 3) {
+		$('#tab-three').tab('show')
+	}
+}	 
+
+function scrollSwitch(newTab) {
+
 	var position = window.getComputedStyle(indicator).getPropertyValue("position");
 	
-	if ((position == 'sticky') && (window.scrollY >= 162)) {
-		window.scrollTo(0, 162);
+	if (tabData[0] == newTab) {
+		tabData[tabData[0]] = 0
+		scrollDistance = 0
 	} else {
-		window.scrollTo(0, 0);
+		tabData[tabData[0]] = window.scrollY
+		scrollDistance = tabData[newTab]
+		if (position == 'sticky') {
+			if (scrollDistance < 162) {
+				if (window.scrollY > 162) {
+					scrollDistance = 162
+				} else {
+					scrollDistance = window.scrollY
+				}
+			}
+		}
 	}
+	
+	tabData[0] = newTab;
+	switchTab(newTab);
+	
+	setTimeout(function(){window.scrollTo(0,scrollDistance);},250);
+	
+	return tabData
 }
+
+
+
+
+
+
+
+
